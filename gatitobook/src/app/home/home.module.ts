2c90130ef42b6cd6aeeea1243575fccd44aad4a1
Débora Rocha <debora.rocha@iatec.com>
2022-07-27 14:28:59 -0300

    Formulário template-driven
    Usando Two-way data binding no login

--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 // toda vez que criar um componente novo é necessário declarar ele no module
 
 @NgModule({
@@ -13,7 +14,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule // para informar ao angular que usari o formulário tipo template
   ],
   exports: [
     HomeComponent //expondo o home e importando no app module posso chamar no appComponent
