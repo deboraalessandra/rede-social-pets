30eedd429933f69e534005124e96bb25a3226f09
Débora Rocha <debora.rocha@iatec.com>
2022-07-31 19:57:35 -0300

    Serviço de token criado

---
+++ gatitobook/src/app/autenticacao/token.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'token'; // vai ser a chave do localstorage
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor() { } // n tem construtor pois não será injetado nenhum serviço nele
+  //alguns metodos para a classe
+  retornaToken() {
+    return localStorage.getItem(KEY) ?? ''; // retornar o localStorage
+  } // se não pehar a chave vai retornar uma variável em branco
+
+  salvaToken(token: string) { // receber o token que vai ser uma string
+    localStorage.setItem(KEY, token); // e setar o token que receber
+  }
+
+  excluiToten() { // par remover o token
+    localStorage.removeItem(KEY);
+  }
+
+  possuiToten() { // retorna uma aplicação q se tem ou não token guardado
+    return !! this.retornaToken(); // retorna uma boolean
+  }
+}
