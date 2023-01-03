10d851ba079d59877ec438fab8ed962b15e9841b
Débora Rocha <debora.rocha@iatec.com>
2023-01-03 10:16:28 -0300

    Comentários no código

--- Front-End/gatitobook/src/app/animais/animais.ts
+++ Front-End/gatitobook/src/app/animais/animais.ts
@@ -1,11 +1,13 @@
-export interface Animal { //propriedades da API
+// Interface de retorno da API
+export interface Animal {
+  //propriedades da API
   id: number;
   postDate: Date; //data da postagem que vem da API
   url: string; //url da foto
   description: string; // Vem da API
   allowComments: boolean; //permitir comentários
   likes: number; //número de likes
-  comments: number;
+  comments: number; //Comentários da foto
   userId: number; //usuário proprietário da foto
 }
 
